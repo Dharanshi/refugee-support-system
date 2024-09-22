3532cd65cf0379d20044b2d0a6142daed160074c
Ramkrishna Swarnkar <93124692+Dharanshi@users.noreply.github.com>
2024-09-22 06:25:58 +0530

    update

---
+++ hardhat.config.ts
@@ -0,0 +1,45 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+      },
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+          viaIR: true, // Add this line
+        },
+      },
+    ],
+  },
+  allowUnlimitedContractSize: true,
+  networks: {
+    hardhat: {
+      chainId: 555,
+    },
+    bsc_test: {
+      url: process.env.BSCTEST,
+      accounts: [process.env.PRIVATE_KEY_TEST],
+    },
+    polygon_main: {
+      url: process.env.POLYGONMAIN,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    sepolia_test: {
+      url: process.env.SEPOLIATEST,
+      accounts: [process.env.PRIVATE_KEY_TEST],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.VERIFYAPI,
+  },
+};
